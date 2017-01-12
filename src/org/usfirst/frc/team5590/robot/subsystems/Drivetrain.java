d774900691e4d1c2f102d2453219e8f1d58779f8
Daniel Starner <starner.daniel5@gmail.com>
2017-01-12 12:31:43 -0800

    Drivetrain methods updated

--- src/org/usfirst/frc/team5590/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team5590/robot/subsystems/Drivetrain.java
@@ -17,12 +17,42 @@ public class Drivetrain extends Subsystem {
     
     
     public Drivetrain(){
-    	robotDrive = new RobotDrive(LEFTCONTROLLERPWM,RIGHTCONTROLLERPWM);
+    	robotDrive = new RobotDrive(LEFTCONTROLLERPWM, RIGHTCONTROLLERPWM);
     }
 
+    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
+    
+    /**
+     * This method will set the speed of the drivetrain 
+     * motors according to the joystick controllers.
+     */
+    public void joystickSpeed() {
+    	
+    }
+    
+    
+    /**
+     * This method will set the speed of the motors
+     * in the drivetrain to whatever *speed* is.
+     */
+    public void setSpeed(double speed) {
+    	
+    }
+    
+    
+    
+    /**
+     * This method will be called to STOP the robot.
+     */
+    public void stop() {
+    	
+    }
+    
+    
 }
 
